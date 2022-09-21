cb5d1e86d26ea926853d862f019a634d2e3cef08
mahant95 <montyempire95@gmail.com>
2022-09-21 22:06:32 +0530

    Added Line Comparision Program

---
+++ src/com/LineComparison/mahant/LineComparision.java
@@ -0,0 +1,12 @@
+package com.LineComparison.mahant;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome to line comparison computation");
+
+	}
+
+}
